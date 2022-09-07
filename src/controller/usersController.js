38f27a114e74351ede23aa10e4791e08dd8e8cee
Daniel <danieldemelorumpf@gmail.com>
2022-09-07 18:42:49 -0300

    GET by id method implemented

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -8,13 +8,22 @@ class UserController {
         user.save((err) => {
             err ? res.status(500).send("Failed to register user, one or more required fields must be filled")
                 : res.status(201).send(user.toJSON())
-        })
+        });
     }
 
     static listUsers = (req, res) => {
         users.find((err, users) => {
             res.status(200).json(users)
-        })
+        });
+    }
+
+    static listUsersById = (req, res) => {
+        const id = req.params.id;
+
+        users.findById(id, (err, users) => {
+                err ? res.status(400).send('ID not found.')
+                    : res.status(200).send(users);
+            });
     }
 
     static updateUser = (req, res) => {
@@ -23,7 +32,7 @@ class UserController {
         users.findByIdAndUpdate(id, {$set: req.body}, (err) => {
             err ? res.status(500).send({message: err.message}) 
                 : res.status(200).send('User updated succefully!')
-        })
+        });
     }
 }
 
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,7 +4,10 @@ import UserController from "../controller/usersController.js";
 const router = express.Router();
 
 router
+
     .get("/api/v1/users", UserController.listUsers)
+    .get("/api/v1/users/:id", UserController.listUsersById)
     .post("/api/v1/users", UserController.registerUser)
     .put("/api/v1/users/:id", UserController.updateUser)
+
 export default router;
