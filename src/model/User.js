a874012c04b773efcd4390a55096198975345bc7
Daniel <danieldemelorumpf@gmail.com>
2022-09-08 02:25:29 -0300

    Modified validate positions

--- src/model/User.js
+++ src/model/User.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const birthDate = () => {
-        
+
     let currentDate = new Date()
     let age = Math.floor((currentDate - birthDate) / 31536000000)
-    
+
     return age > 17 ? true : false;
 
 }
@@ -12,21 +12,22 @@ const birthDate = () => {
 const userSchema = new mongoose.Schema(
     {
         id: { type: String },
-        name: { type: String, validate: /[A-zÀ-ú\s]+$/, required: true },
-        cpf: { type: String, validate: /^[0-9]*$/, minlength: 11, maxlength: 11, required: true },
-        birthDate: { type: Date, required: true, validate: [birthDate]},
-        email: { type: String, validate: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, required: true },
+        name: { type: String, required: true, validate: /[A-zÀ-ú\s]+$/ },
+        cpf: { type: String, minlength: 11, maxlength: 11, required: true, validate: /^[0-9]*$/ },
+        birthDate: { type: Date, required: true, validate: [birthDate] },
+        email: { type: String, required: true, validate: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/ },
         password: { type: String, minlength: 6, required: true },
         address: { type: String, required: true, validate: /[A-zÀ-ú\s]/ },
-        number: { type: String, validate: /^[0-9]*$/, required: true },
+        number: { type: String, required: true, validate: /^[0-9]*$/ },
         complement: { type: String, required: true },
-        city: { type: String, validate: /[A-zÀ-ú\s]/, required: true },
-        state: { type: String, validate: /[A-zÀ-ú\s]/, required: true },
-        country: { type: String, validate: /[A-zÀ-ú\s]/, required: true },
-        zipCode: { type: String, validate: /^[0-9]*$/, minlength: 8, maxlength: 8, required: true }
+        city: { type: String, required: true, validate: /[A-zÀ-ú\s]/ },
+        state: { type: String, required: true, validate: /[A-zÀ-ú\s]/ },
+        country: { type: String, required: true, validate: /[A-zÀ-ú\s]/ },
+        zipCode: { type: String, minlength: 8, maxlength: 8, required: true, validate: /^[0-9]*$/ }
     },
     {
         versionKey: false
+
     }
 );
 
